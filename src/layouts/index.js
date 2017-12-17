d4dc61a2e7b73e052a266a365a11b0c8d5e3c0cc
Ariel Gerstein <ariel.ger@hotmail.com>
2017-12-17 18:43:49 -0300

    Add bootstrap grid and reboot css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,35 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
-
-import "./index.css";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 const Header = () => (
-  <div
-    style={{
-      background: "rebeccapurple",
-      marginBottom: "1.45rem"
-    }}
-  >
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "1.45rem 1.0875rem"
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: "white",
-            textDecoration: "none"
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
+  <div>
+    <h1>Header</h1>
   </div>
 );
 
@@ -40,25 +17,16 @@ const TemplateWrapper = ({ children }) => (
       title="Gatsby Default Starter"
       meta={[
         { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
+        { name: "keywords", content: "sample, something" },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
 );
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func
+  children: PropTypes.func,
 };
 
 export default TemplateWrapper;
