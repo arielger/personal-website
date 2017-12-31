d0bfd9c66335e5b44265ab10d728174e23cb1448
Ariel Gerstein <ariel.ger@hotmail.com>
2017-12-31 15:26:52 -0300

    Change copy of 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+      <h1>Page not found ⚠️</h1>
+      <p>You just hit a route that doesn&#39;t exist.</p>
+    </div>
   </div>
 );
 
